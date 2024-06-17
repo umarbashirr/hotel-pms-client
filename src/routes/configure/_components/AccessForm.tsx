a51ec8f233431875bde30cbf378f4ea5783fd8f9
umarbashirr <umarbashir93@outlook.com>
2024-06-17 15:51:11 +0530

    refactor: Remove commented code in AccessForm.tsx

--- src/routes/configure/_components/AccessForm.tsx
+++ src/routes/configure/_components/AccessForm.tsx
@@ -32,8 +32,6 @@ const AccessForm = ({ isEditing, user }: AccessFormProps) => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
